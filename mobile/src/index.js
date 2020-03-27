c03d658f344add08f0f5dea97fc6ce956b44dcec
Luan Pires Bresolla <luanpires34@gmail.com>
2020-03-27 12:07:16 -0300

    Finalizando aplicação Web

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,16 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import 'react-native-gesture-handler';
 
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar backgroundColor="#13131a" />
+      <Routes />
+    </>
+  );
 }
--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,10 +1,95 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {View, Text, TouchableOpacity, Image, Linking} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import {sendEmail} from '../../services/sendMail';
+
+import styles from './styles';
+import logo from '../../assets/logo.png';
 
 export default function Detail() {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const incident = route.params.incident;
+
+  const message = `Olá ${
+    incident.name
+  }, estou entrando em contato pois gostaria de ajudar no caso "${
+    incident.title
+  }" com o valor de ${Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(incident.value)}`;
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  function sendMailHero() {
+    sendEmail(
+      `${incident.email}`,
+      `Herói do caso: ${incident.title}`,
+      message,
+    ).then(() => {
+      console.log('Our email successful provided to devide mail');
+    });
+  }
+
+  function sendWhatsaap() {
+    Linking.openURL(
+      `whatsapp://send?text=${message}&phone=${incident.whatsaap}`,
+    );
+  }
+
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} />
+
+        <TouchableOpacity onPress={navigateBack}>
+          <Icon name="arrowleft" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+        <Text style={styles.incidentValue}>
+          {incident.name} de {incident.city}/{incident.uf}
+        </Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>{incident.title}</Text>
+
+        <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+        <Text style={styles.incidentValue}>{incident.description}</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(incident.value)}
+        </Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entrar em contato</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsaap}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={sendMailHero}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
