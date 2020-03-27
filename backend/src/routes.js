8f3f73823686633563fe3a81bb7a053e7aa569fb
Luan Pires Bresolla <luanpires34@gmail.com>
2020-03-27 17:35:04 -0300

    Finalizando Semana OmniStack

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,45 @@
-import { Router } from 'express';
+const { Router } = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
-import OngController from './controllers/OngController';
-import IncidentController from './controllers/IncidentController';
-import ProfileController from './controllers/ProfileController';
-import SessionController from './controllers/SessionController';
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = Router();
 
 routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(15),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.store);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}) , ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}) ,IncidentController.index);
+
+
 routes.post('/incidents', IncidentController.store);
-routes.delete('/incidents/:id', IncidentController.delete);
 
-export default routes;
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}) ,IncidentController.delete);
+
+module.exports = routes;
